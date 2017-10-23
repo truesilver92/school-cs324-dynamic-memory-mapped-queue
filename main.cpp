a0a9610ddbefb946d19733746c456ab5e56e905a
Joel Stoker <joel.d.stoker@gmail.com>
2017-10-23 15:43:55 -0600

    finished arrayqueue implementation

--- main.cpp
+++ main.cpp
@@ -19,25 +19,62 @@ double When()
 class ArrayQueue {
 public:
 
+  ulong max_size = 4096;
+  int *array;
+  ulong head;
+  ulong tail;
+  
+
   ArrayQueue(){
+
+    array = new int[max_size];
     
+    // set the head and tail to the begining
+    head = (ulong)array;
+    tail = (ulong)array;
   }
 
   // add item
   void enqueue(int item) {
-    
+    *(int*)tail = item;
+    // move the tail forward and wrap around if at end
+    tail = (tail + 1) % max_size;
   }
 
   // remove item
   int dequeue() {
-    
+    int item = *(int*)head;
+    // clear it
+    *(int*)head = 0;
+    head = (head + 1) % max_size;
+    return item;
   }
 };
+
+void arrayqueue_test(ArrayQueue &aq, int number_to_do){
+  for(int i = 0; i < number_to_do; i++){
+    aq.enqueue(1);
+  }
+  for(int i = 0; i < number_to_do; i++){
+    aq.dequeue();
+  }
+}
 
 int main(){
   // run and test array implementation
   // create array version
-  
+  ArrayQueue aq = ArrayQueue();
+  double time = When();
+  arrayqueue_test(aq, 100);
+  double time2 = When();
+  std::cout << "ArrayQueue test 100 time:\n";
+  std::cout << time2 - time;
+
+  time = When();
+  arrayqueue_test(aq, 1000);
+  time2 = When();
+  std::cout << "ArrayQueue test 1000 time:\n";
+  std::cout << time2 - time;
 
   // run and test linked list version
 
