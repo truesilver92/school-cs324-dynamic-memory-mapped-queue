a45563929e44e09959826b3fa933cf1d18a7674d
Joel Stoker <joel.d.stoker@gmail.com>
2017-10-22 22:19:38 -0600

    added some more base code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
-// first line
+
+#include <sys/time.h>
+
+/* Return the current time in seconds, using a double precision number.       */
+
+double When()
+
+{
+
+    struct timeval tp;
+
+    gettimeofday(&tp, 0);
+
+    return ((double) tp.tv_sec + (double) tp.tv_usec * 1e-6);
+
+}
+
+class queue {
+public:
+  void enqueue(int item) {
+    
+  }
+};
+
+int main(){
+  // run and test array implementation
+  // create array version
+  
+
+  // run and test linked list version
+
+  // run and test memory mapped version
+}
---
+++ makefile
@@ -0,0 +1,19 @@
+C = g++
+FLAGS = -g -D_DEBUG -Wall
+LFLAGS = $(FLAGS) -o
+CFLAGS = $(FLAGS) -c
+
+CPPSOURCES = $(wildcard *.cpp)
+HSOURCES = $(wildcard *.h)
+OBJECTS = $(CPPSOURCES:.cpp=.o)
+
+debug : mmq
+
+mmq: $(OBJECTS)
+	$(C) $(LFLAGS) mmq $^
+
+# object building rules
+
+clean:
+	touch .o; rm -rf *.o; rm mmq; touch vgcore; rm vgcore*
+
